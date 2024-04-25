e95e1ea70d0b2d6b288ca1ba6bf401ef6d649dd9
Pranav Barthwal <110532770+PranavBarthwal@users.noreply.github.com>
2024-04-25 05:22:51 +0000

    add mui tab section

--- src/components/Dashboard/Tabs/Tabs.jsx
+++ src/components/Dashboard/Tabs/Tabs.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-function TabsComponent() {
-    return (
-        <h1>Tabs</h1>
-    )
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+
+export default function TabsComponent() {
+  const [value, setValue] = useState('grid');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div>
+      <TabContext value={value}>
+        <TabList 
+            onChange={handleChange} 
+            aria-label="lab API tabs example"
+            variant="fullWidth"
+            defaultView="grid"
+        >
+            <Tab label="Grid View" value="grid" style={{ color: 'white' }} />
+            <Tab label="List View" value="list" style={{ color: 'white' }} />
+        </TabList>
+        <TabPanel value="grid" style={{ color: 'white' }}>Item One</TabPanel>
+        <TabPanel value="list" style={{ color: 'white' }}>Item Two</TabPanel>
+      </TabContext>
+    </div>
+  );
 }
-
-export default TabsComponent
